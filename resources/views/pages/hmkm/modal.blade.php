cacbad63ae7bb1b40028d00b1d8e5b86095838d9
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-06-17 12:13:46 +0700

    Update modal.blade.php

--- resources/views/pages/hmkm/modal.blade.php
+++ resources/views/pages/hmkm/modal.blade.php
@@ -87,7 +87,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="qrScannerModalLabel">QR Code Scanner</h5>
+                    <h5 class="modal-title" id="qrScannerModalLabel">SCAN QR UNIT</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
