e0f569c2072dfde569c7273c4048b0cca6ee23ab
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-06-22 04:28:13 +0700

    role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['role:admin']);
+    }
+
+
     /**
      * Display a listing of the resource.
      */
