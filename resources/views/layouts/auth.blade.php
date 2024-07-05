0718c50fe10a506142f114fc11b1e3413aaa4167
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-07-05 15:35:36 +0700

    add favicon

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,6 +6,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }} &mdash; {{ env('APP_NAME') }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/stisla.svg') }}" />
 
     <!-- General CSS Files -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
