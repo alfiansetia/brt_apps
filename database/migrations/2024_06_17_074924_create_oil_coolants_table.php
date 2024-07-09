4b2681a5a4230ffb47fc896edbf0f33d835c6303
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-07-09 14:33:58 +0700

    Update 2024_06_17_074924_create_oil_coolants_table.php

--- database/migrations/2024_06_17_074924_create_oil_coolants_table.php
+++ database/migrations/2024_06_17_074924_create_oil_coolants_table.php
@@ -14,15 +14,15 @@ return new class extends Migration
         Schema::create('oil_coolants', function (Blueprint $table) {
             $table->id();
             $table->date('date')->useCurrent();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('product_id')->nullable();
             $table->unsignedBigInteger('unit_id');
             $table->decimal('amount', 12, 2)->default(0);
             $table->enum('type', ['levelling', 'service'])->default('levelling');
             $table->string('desc')->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
-            $table->foreign('product_id')->references('id')->on('products')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('user_id')->references('id')->on('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreign('product_id')->references('id')->on('products')->nullOnDelete()->cascadeOnUpdate();
             $table->foreign('unit_id')->references('id')->on('units')->cascadeOnDelete()->cascadeOnUpdate();
         });
     }
