6316b502d0ab48f003360b8ae66507b437e078cc
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-07-11 15:56:59 +0700

    add to offline
    fontawesome, bs-customfileinput
    jquery, bootstrap, blockui, poper

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,10 +9,8 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('assets/img/stisla.svg') }}" />
 
     <!-- General CSS Files -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('lib/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('lib/@fortawesome/fontawesome-free/css/all.css') }}">
     <link rel="stylesheet" href="{{ asset('lib/bootstrap-daterangepicker/daterangepicker.css') }}">
 
     @stack('css')
@@ -94,21 +92,17 @@
     </form>
 
     <!-- General JS Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="{{ asset('lib/jquery/dist/jquery.js') }}"></script>
+    <script src="{{ asset('lib/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('lib/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('lib/jquery.nicescroll/dist/jquery.nicescroll.min.js') }}"></script>
+
     <script src="{{ asset('lib/moment/min/moment.min.js') }}"></script>
     <script src="{{ asset('assets/js/stisla.js') }}"></script>
 
     <script src="{{ asset('lib/izitoast/dist/js/iziToast.min.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/block-ui@2.70.1/jquery.blockUI.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/block-ui@2.70.1/jquery.blockUI.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bs-custom-file-input/dist/bs-custom-file-input.min.js"></script>
+    <script src="{{ asset('lib/blockui-2.70/jquery.blockUI.js') }}"></script>
+    <script src="{{ asset('lib/bs-custom-file-input-1.3.4/dist/bs-custom-file-input.min.js') }}"></script>
     <script src="{{ asset('lib/sweetalert/dist/sweetalert.min.js') }}"></script>
     <script src="{{ asset('lib/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
 
