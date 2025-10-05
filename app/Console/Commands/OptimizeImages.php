f8a3727dd23515ddfd0839e37353d7a98ae024a8
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-10-05 22:16:34 +0700

    update intervension

--- app/Console/Commands/OptimizeImages.php
+++ app/Console/Commands/OptimizeImages.php
@@ -38,88 +38,40 @@ class OptimizeImages extends Command
     public function handle()
     {
 
-        $this->info("🧩 Starting image optimization...\n");
+        ini_set('memory_limit', '4098M');
+        foreach ($this->folders as $folder) {
+            if (file_exists(storage_path($folder))) {
+                $path = storage_path($folder);
 
-        $source = storage_path('app/public/img/part');
-        $destination = storage_path('app/public/img/part/tmp');
+                if (!File::exists($path)) {
+                    $this->warn("⚠️ Folder not found: $folder");
+                    continue;
+                }
 
-        if (!File::exists($destination)) {
-            File::makeDirectory($destination, 0755, true);
-        }
+                $files = File::allFiles($path);
+                $this->info("📁 Optimizing folder: $folder (" . count($files) . " file(s))");
 
-        $files = File::allFiles($source);
-
-        foreach ($files as $key => $file) {
-            try {
-                $this->info("🎉 File: " . $file->getFilename());
-
-                $image = Image::read($file->getRealPath());
-
-                $image->scaleDown(800);
-
-                $savePath = $destination . '/' . $file->getFilename();
-                $image->save($savePath);
-
-                $this->line("✅ Saved to: " . $savePath);
-            } catch (\Throwable $e) {
-                $this->error("❌ Error on " . $file->getFilename() . " → " . $e->getMessage());
+                foreach ($files as $key => $file) {
+                    $filePath = $file->getRealPath();
+                    $this->info("Process: " . $filePath);
+                    try {
+                        $image = Image::read($filePath);
+                        $width = $image->width();
+                        if ($width > $this->maxWidth) {
+                            $image->scaleDown(width: $this->maxWidth);
+                            $image->save($filePath, quality: $this->quality);
+                            $this->info("✅ Optimized: " . $file->getFilename());
+                        } else {
+                            $this->error("↩️ Skipped (already small): " . $file->getFilename());
+                        }
+                    } catch (\Throwable $e) {
+                        $this->error("❌ Failed: {$file->getFilename()} - " . $e->getMessage());
+                    }
+                }
+                $this->newLine();
             }
         }
-
-        // $this->info("🧩 Starting image optimization...\n");
-        // $files = File::allFiles(storage_path('/app/public/img/part'));
-        // foreach ($files as $key => $file) {
-        //     $this->info("🎉 File :" . $file->getFilename());
-        //     $image = Image::read($file);
-        //     $image->orient();
-        //     $image->scaleDown(800);
-        //     $image->save(storage_path('/app/public/img/part/tmp') . $file->getFilename());
-        // }
-
-        // foreach ($this->folders as $folder) {
-        //     if (file_exists(storage_path($folder))) {
-        //         $path = storage_path($folder);
-
-        //         if (!File::exists($path)) {
-        //             $this->warn("⚠️ Folder not found: $folder");
-        //             continue;
-        //         }
-
-        //         $files = File::allFiles($path);
-        //         $this->info("📁 Optimizing folder: $folder (" . count($files) . " file(s))");
-
-        //         foreach ($files as $key => $file) {
-        //             $filePath = $file->getRealPath();
-
-        //             // 🧠 Cek MIME type
-        //             // $mime = @mime_content_type($filePath);
-        //             // if (!str_starts_with($mime, 'image/')) {
-        //             //     $this->info("🚫 Skipped (not image): " . $file->getFilename());
-        //             //     continue;
-        //             // }
-        //             $this->info("Process: " . $filePath);
-
-        //             // try {
-        //             $image = scaleDown($filePath);
-        //             $image->save($filePath);
-
-        //             // $image = Image::read($filePath);
-        //             // $width = $image->width();
-        //             // if ($width > $this->maxWidth) {
-        //             //     $image->scaleDown(width: $this->maxWidth);
-        //             //     $image->save($filePath, quality: $this->quality);
-        //             //     $this->info("✅ Optimized: " . $file->getFilename());
-        //             // } else {
-        //             //     $this->error("↩️ Skipped (already small): " . $file->getFilename());
-        //             // }
-        //             // } catch (\Throwable $e) {
-        //             //     $this->error("❌ Failed: {$file->getFilename()} - " . $e->getMessage());
-        //             // }
-        //         }
-        //         $this->newLine();
-        //     }
-        // }
-        // $this->info("🎉 Optimization complete!");
+        $this->info("🎉 Optimization complete!");
         return 0;
     }
 }
